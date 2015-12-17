b56fff336cf4022d25f5c2128a446e4cb03fc8ad
Collier Devlin <jocodev@gmail.com>
2015-12-16 23:02:04 -0500

    Fixed promise issue. Added calories to chart.

--- scripts/app.js
+++ scripts/app.js
@@ -23,18 +23,13 @@ fitbit.config(function($stateProvider, $urlRouterProvider) {
         });
     $urlRouterProvider.otherwise('/');
 })
-.run(function ($rootScope, $location) {
+.run(function ($rootScope, $location, Session) {
 
 	$rootScope.isLoggedIn = function () {
 
-		var user_info = {};
-
-        if (JSON.parse(window.localStorage.getItem("fitbit"))){
-            user_info.accessToken   = JSON.parse(window.localStorage.getItem("fitbit")).oauth.access_token;
-        }
-
-   		if (user_info.accessToken == null || user_info.accessToken === 0) {
+   		if (Session.accessToken == null || Session.accessToken === 0) {
    			$location.path( "/" );
+
             return false;
    		} else {
 
@@ -53,87 +48,82 @@ fitbit.controller("LoginController", function($scope) {
  
 });
  
-fitbit.controller("DashboardController", function($rootScope, $scope, $http, $location, $filter) {
+fitbit.controller("DashboardController", function($rootScope, $scope, $http, $location, $filter, Session, FitBitAPI) {
 
 	if ($rootScope.isLoggedIn()) {
-    
+
         $scope.user_info = {};
+        $scope.activity = [];
 
-        $scope.user_info.accessToken    = JSON.parse(window.localStorage.getItem("fitbit")).oauth.access_token;
-        $scope.user_info.expiresIn      = JSON.parse(window.localStorage.getItem("fitbit")).oauth.expires_in;
-        $scope.user_info.accountUserId  = JSON.parse(window.localStorage.getItem("fitbit")).oauth.account_user_id;
+        $scope.user_info.accessToken    = Session.accessToken;
+        $scope.user_info.expiresIn      = Session.expiresIn;
+        $scope.user_info.accountUserId  = Session.accountUserId;
 
-        $scope.profileData = function () {
+        $scope.profileData = FitBitAPI.getProfileData();
+        $scope.profileData.then(function (data) {
 
-            if ($rootScope.isLoggedIn()) {
+            var user = data[0].data.user;
 
-                $http({
-                  method  : 'GET',
-                  url     : 'https://api.fitbit.com/1/user/' + $scope.user_info.accountUserId +'/profile.json',
-                  headers : {'Authorization': 'Bearer ' + $scope.user_info.accessToken}
-                })
-                .success(function(data) {
-                    if (data.errors) {
-                        alert('ERRORS');
-                    } else {
-                        $scope.avatar = data.user.avatar150;
-                        $scope.name = data.user.fullName;
-                        $scope.gender = data.user.gender;
-                        $scope.averageDailySteps = data.user.averageDailySteps;
-                    }
-                });
+            $scope.avatar = user.avatar150;
+            $scope.name = user.fullName;
+            $scope.gender = user.gender;
+            $scope.averageDailySteps = user.averageDailySteps;
 
+        });
+
+        // For step chart
+        $scope.labels = [];
+        $scope.data = [[],[]];
+
+        $scope.today = new Date();
+        
+        var dates = [];
+        var past_days = 4;
+
+        for (var i=1; i<=past_days; i++) {
+            var curr_date = new Date();
+            curr_date.setDate($scope.today.getDate() - (past_days - i));
+            curr_date = $filter('date')(curr_date, "yyyy-MM-dd");
+
+            dates.push(curr_date);
+
+            curr_date = $filter('date')(curr_date, "EEE d");
+            $scope.labels.push(curr_date);
+        }
+
+        $scope.activityData = FitBitAPI.getActivityData(dates);
+        $scope.activityData.then(function (data){
+
+            var activities = [];
+            var idx = 0;
+
+            angular.forEach(data, function(value, key) {
+
+                var activity = value.data.summary;
+                var info = {};
+                
+                info.user_id = Session.accountUserId;
+                info.steps = activity.steps;
+                info.calories = activity.caloriesOut;
+                info.caloriesBMR = activity.caloriesBMR;
+                
+                activities.push(info);
+                activities[idx].day = dates[idx];
+
+                idx++;
+            });
+
+            for (var i = 0; i < activities.length; i++) {
+                $scope.activity[i] = activities[i];
+                $scope.data[0].push($scope.activity[i].steps);
+                $scope.data[1].push($scope.activity[i].calories);
             }
-        };
 
-        $scope.activityData = function ($dates, $is_today) {
-
-             if ($rootScope.isLoggedIn()) {
-                angular.forEach($dates, function ($date) {
-                    $http({
-                      method  : 'GET',
-                      url     : 'https://api.fitbit.com/1/user/' + $scope.user_info.accountUserId +'/activities/date/' + $date + '.json',
-                      headers : {'Authorization': 'Bearer ' + $scope.user_info.accessToken}
-                    })
-                    .success(function(data) {
-                        if (data.errors) {
-                            alert('ERRORS');
-                        } else {
-                            if (!$is_today) {
-                                $scope.activity.push({
-                                    "Day": $date,
-                                    "Steps": data.summary.steps,
-                                    "Calories": data.summary.caloriesOut,
-                                    "Calories BMR": data.summary.caloriesBMR
-                                });
-
-                                var $date_copy = $filter('date')($date, "EEE d");
-
-                                $scope.labels.push($date_copy);
-                                $scope.data[0].push(data.summary.steps);
-
-                                console.log($scope.labels);
-                                console.log($scope.data[0]);
-                            }
-                            else
-                            {
-                                $scope.todayActivity.push({
-                                    "Day": $date,
-                                    "Steps": data.summary.steps,
-                                    "Calories": data.summary.caloriesOut,
-                                    "Calories BMR": data.summary.caloriesBMR
-                                });
-                            }
-                        }
-                    });
-
-                });
-            }       
-        };
+        });
 
         $scope.stepChart = function () {
 
-            $scope.series = ['Steps'];
+            $scope.series = ['Steps', 'Calories Burned'];
 
             $scope.onClick = function (points, evt) {
                 console.log(points, evt);
@@ -141,35 +131,58 @@ fitbit.controller("DashboardController", function($rootScope, $scope, $http, $lo
 
         };
 
-        angular.element(document).ready(function () {
-
-            $scope.activity = [];
-            $scope.todayActivity = [];
-
-            // For step chart
-            $scope.labels = [];
-            $scope.data = [[]];
-
-            $scope.today = new Date();
-            $scope.past7 = [];
-
-            for (var i=1; i<=7; i++) {
-                var curr_date = new Date();
-                curr_date.setDate($scope.today.getDate() - i);
-                curr_date = $filter('date')(curr_date, "yyyy-MM-dd");
-
-                $scope.past7.push(curr_date);
-            }
-
-            $scope.today = $filter('date')($scope.today, "yyyy-MM-dd");
-            $scope.past7.push($scope.today);
-
-    		$scope.profileData();
-            $scope.activityData([$scope.today], true);
-            $scope.activityData($scope.past7);
-            $scope.stepChart();
-    	});
+        $scope.stepChart();
     }
+});
 
- 
+fitbit.factory('Session', function() {
+
+    var access_token;
+    var expires_in;
+    var account_user_id;
+
+    if (JSON.parse(window.localStorage.getItem("fitbit"))) {
+        access_token = JSON.parse(window.localStorage.getItem("fitbit")).oauth.access_token;
+        expires_in = JSON.parse(window.localStorage.getItem("fitbit")).oauth.expires_in;
+        account_user_id = JSON.parse(window.localStorage.getItem("fitbit")).oauth.account_user_id;
+    }
+    
+    return {
+       accessToken: access_token,
+       expiresIn: expires_in,
+       accountUserId: account_user_id
+    }
+});
+
+fitbit.factory('FitBitAPI', function($http, $q, Session) {
+
+    return {
+        getProfileData: function() {
+
+            var info = $q.defer();
+
+            info = $http({
+                method  : 'GET',
+                url     : 'https://api.fitbit.com/1/user/' + Session.accountUserId +'/profile.json',
+                headers : {'Authorization': 'Bearer ' + Session.accessToken}
+            })
+
+            return $q.all([info]);
+        },
+        getActivityData: function($dates) {
+
+            var promises = [];
+
+            angular.forEach($dates, function(date, key){
+                promises.push($http({
+                    method  : 'GET',
+                    url     : 'https://api.fitbit.com/1/user/' + Session.accountUserId +'/activities/date/' + date + '.json',
+                    headers : {'Authorization': 'Bearer ' + Session.accessToken}
+                    })
+                );
+            });
+
+            return $q.all(promises);
+        }
+    }
 });
